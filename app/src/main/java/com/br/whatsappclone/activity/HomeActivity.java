116832be0d334ec2bd59947613bd198efa3741f8
Antonio Marcos dos Santos de Souza <71250901+Antonio-Marcos86@users.noreply.github.com>
2021-06-04 17:35:44 -0300

    Inserindo imagem arredondada

--- app/src/main/java/com/br/whatsappclone/activity/HomeActivity.java
+++ app/src/main/java/com/br/whatsappclone/activity/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -62,8 +63,15 @@ public class HomeActivity extends AppCompatActivity {
                 deslogarUsuario();
                 finish();
                 break;
+            case R.id.menuConfiguracao:
+                abrirConfiguracoes();
+                break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void abrirConfiguracoes() {
+        startActivity(new Intent(this,ConfiguracoesActivity.class));
     }
 
     private void deslogarUsuario() {
